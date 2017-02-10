4efba46c8d6b66fd4d64c6610f3796dea676093e
Lewis Hoffman <lewis.hoffman@gmail.com>
2017-02-09 22:27:45 -0700

    dotfiles for chruby

--- dotfiles/bash_rc.d/alias.sh
+++ dotfiles/bash_rc.d/alias.sh
@@ -1,3 +1,5 @@
 alias kak='killall -w autokey-gtk; autokey-gtk & >/dev/null 2>&1'
 alias nocaps='/usr/bin/setxkbmap -option "ctrl:nocaps"'
 alias togglecaps='xdotool key Caps_Lock'
+alias gl="git for-each-ref --count=10 --sort=-committerdate refs/heads/ --format='%(refname:short)'"
+
--- dotfiles/bash_rc.d/custom.sh
+++ dotfiles/bash_rc.d/custom.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-# Add RVM to PATH for scripting
-export PATH="$PATH:$HOME/.rvm/bin"
-
 
 # add sphinx path for glassfrog
 export SPHINX_PATH="/usr/bin"
 
-#source /home/lewis/.rvm/scripts/rvm
- [[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function
 
 export PATH="$PATH:$HOME/.local/bin"
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
+
