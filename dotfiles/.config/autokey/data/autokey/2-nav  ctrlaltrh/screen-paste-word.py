fa8076d000945df9217378c0795514ad9b5efbc7
Lewis Hoffman <lewis.hoffman@gmail.com>
2017-03-02 20:26:04 -0700

    finish tweaks for screen

--- dotfiles/.config/autokey/data/autokey/2-nav  ctrlaltrh/screen-paste-word.py
+++ dotfiles/.config/autokey/data/autokey/2-nav  ctrlaltrh/screen-paste-word.py
@@ -1,6 +1,7 @@
 
 if re.match('.*terminal.*', window.get_active_class()):
+    keyboard.send_keys("W")
     keyboard.send_keys("<ctrl>+a")
-    keyboard.send_keys("]")                  
+    keyboard.send_keys("]")                      
 else:
     keyboard.send_keys("<alt>+<down>")
