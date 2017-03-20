f1f31b8b1c5b89a8608c5e44411aa5ab9c5e2c60
Lewis Hoffman <lewis.hoffman@gmail.com>
2017-03-19 19:19:06 -0600

    moar keybindings

--- dotfiles/bash_rc.d/alias.sh
+++ dotfiles/bash_rc.d/alias.sh
@@ -2,5 +2,5 @@ alias kak='killall -w autokey-gtk; autokey-gtk & >/dev/null 2>&1'
 alias nocaps='/usr/bin/setxkbmap -option "ctrl:nocaps"'
 alias togglecaps='xdotool key Caps_Lock'
 alias gl="git for-each-ref --count=10 --sort=-committerdate refs/heads/ --format='%(refname:short)'"
-alias s="screen"
+alias s="spring"
 
