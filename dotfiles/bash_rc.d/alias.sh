af20a194325327686fd5d81dd1402b0733774aa3
Lewis Hoffman <lewis.hoffman@gmail.com>
2018-02-23 20:40:42 -0700

    fix ks

--- dotfiles/bash_rc.d/alias.sh
+++ dotfiles/bash_rc.d/alias.sh
@@ -3,7 +3,7 @@ alias nocaps='/usr/bin/setxkbmap -option "ctrl:nocaps"'
 alias togglecaps='xdotool key Caps_Lock'
 alias gl="git for-each-ref --count=10 --sort=-committerdate refs/heads/ --format='%(refname:short)'"
 alias s="./bin/spring"
-alias ks="ps auxw | grep spring | grep -v grep | awk '{print $2}' | xargs kill -9"
+alias ks="ps auxw | grep spring | grep -v grep | awk '{print \$2}' | xargs kill -9"
 alias kc="kubectl"
 alias kz="killall -9 zoom"
 
